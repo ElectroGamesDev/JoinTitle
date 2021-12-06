d9a812f1a188fd60aee29124793631f937f4a50f
Electro Games <2cool4u567@gmail.com>
2021-12-06 10:49:44 -0600

    Create JoinTitle.php

---
+++ src/Electro/JoinTitle/JoinTitle.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Electro\JoinTitle;
+
+use pocketmine\event\player\PlayerLoginEvent; 
+use pocketmine\plugin\PluginBase; 
+use pocketmine\player\Player; 
+use pocketmine\event\Listener;
+
+class JoinTitle extends PluginBase implements Listener{
+  
+    public string popupMsg;
+    public string titleMsg;
+
+    public function onEnable(): void
+    {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->titleMsg = $this->getServer()->getConfig()->get("TitleMessage");
+        $thid->popupMsg = $this->getServer()->getConfig()->get("TitleMessage"):
+    }
+  
+    public function onLogin(PlayerLoginEvent $event) 
+    { 
+        $player = $event->getPlayer();
+      
+        $player->sendTitle($this->titleMsg);
+        $player->sendPopup($this->popupMsg);
+    }
+  
+}
